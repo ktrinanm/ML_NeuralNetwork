bf4f930d1b1614da7607df2572cebc6672bc4666
Justin Thomas <justamnew@gmail.com>
2018-03-14 12:52:31 -0600

    Add files via upload
    Adds the ability to shuffle the data around. Uses two ArrayLists of double[]'s, one for the testing data and one for the training data. Also contains an ArrayList<Integer> for the training and testing data classifications.

--- ClassificationNeuralNetwork.java
+++ ClassificationNeuralNetwork.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 import java.lang.Integer;
 
 public class ClassificationNeuralNetwork
@@ -6,11 +7,19 @@ public class ClassificationNeuralNetwork
 	public static int [] classification;
 	public static double [] radii, textures, perimeters, area, smoothness,
 		   compactness, concavity, concavePnts, symmetry, fractalDim;
+	public static ArrayList<double[]> listOfTrainingData;
+	public static ArrayList<Integer> listOfTrainingClassifications;
+	
+	public static ArrayList<double[]> listOfTestData;
+	public static ArrayList<Integer> listOfTestClassifications;
+	
+	public static int numLines=569;
 	
 	public static void main(String [] args)
 	{
-		int numLines = 569;
 		classification = new int[numLines];
+		
+		//The 10 factors we're accounting for.
 		radii = new double[numLines];
 		textures = new double[numLines];
 		perimeters = new double[numLines];
@@ -22,6 +31,17 @@ public class ClassificationNeuralNetwork
 		symmetry = new double[numLines];
 		fractalDim = new double[numLines];
 
+		listOfTrainingData= new ArrayList<double[]>();
+		listOfTrainingClassifications= new ArrayList<Integer>();
+		
+		listOfTestData= new ArrayList<double[]>();
+		listOfTestClassifications= new ArrayList<Integer>();
+		
+		putDataInArrayList();
+		
+		//Every day I'm shufflin'
+		shuffle();
+		
 	}
 
 	public static void readDataFromFile()
@@ -61,5 +81,42 @@ public class ClassificationNeuralNetwork
 			System.out.println("Error reading data from file at line "
 					+ currLineNum);
 		}
+	}
+	
+	public static void putDataInArrayList()
+	{
+		for(int i=0; i<numLines; i++)
+		{
+			double[] toAdd= new double[10];
+			
+			toAdd[0]=radii[i];
+			toAdd[1]=textures[i];
+			toAdd[2]=perimeters[i];
+			toAdd[3]=area[i];
+			toAdd[4]=smoothness[i];
+			toAdd[5]=compactness[i];
+			toAdd[6]=concavity[i];
+			toAdd[7]=concavePnts[i];
+			toAdd[8]=symmetry[i];
+			toAdd[9]=fractalDim[i];
+			
+			listOfTestData.add(toAdd);
+			listOfTestClassifications.add(classification[i]);
+		}
+	}
+	
+	public static void shuffle()
+	{
+		int eightyPercentOfData= (int) (listOfTestData.size()*.8);
+		
+		for(int i=0; i<eightyPercentOfData; i++)
+		{
+			int toRemove= (int) (Math.random()*listOfTestData.size());
+			listOfTrainingData.add(listOfTestData.remove(toRemove));
+			listOfTrainingClassifications.add(listOfTestClassifications.remove(toRemove));
+		}
+		
+		System.out.println("For reference, the size of the testing data is " + listOfTestData.size() + ", and the testing classification size is " + listOfTestClassifications.size() + ", which should be the same.");
+		System.out.println("For reference, the size of the training data is " + listOfTrainingData.size() + ", and the training classification size is " + listOfTrainingClassifications.size() + ", which should be the same.");
 	}
 }
